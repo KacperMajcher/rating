8f6c2086822bd49768b1017f096dc0f6f24cd43e
Kacper Majcher <kacpermajcher13@gmail.com>
2023-11-02 15:10:47 +0100

    Add new deadlines

--- lib/repositories/deadline_repository.dart
+++ lib/repositories/deadline_repository.dart
@@ -5,51 +5,23 @@ class DeadlineRepository {
     return [
       DeadlineItem(
         id: 0,
-        task: 'Connect artists to API server',
-        deadline: 0,
-        isDone: true,
+        task: 'Add ArtistModel tests',
+        deadline: 7,
       ),
       DeadlineItem(
         id: 1,
-        task: 'Add Json Serailizable',
-        deadline: 0,
-        isDone: true,
+        task: 'Add ArtistsRepository tests',
+        deadline: 7,
       ),
       DeadlineItem(
         id: 2,
-        task: 'Implement freezed to the project',
-        deadline: 1,
-        isDone: true,
+        task: 'Add ArtistRemoteDataSource tests',
+        deadline: 7,
       ),
       DeadlineItem(
         id: 3,
-        task: 'Implement retrofit to the project',
-        deadline: 1,
-        isDone: true,
-      ),
-      DeadlineItem(
-        id: 4,
-        task: 'Add get_it to the project',
-        deadline: 2,
-        isDone: true,
-      ),
-      DeadlineItem(
-        id: 5,
-        task: 'Add  injectable to the project',
-        deadline: 2,
-        isDone: true,
-      ),
-      DeadlineItem(
-        id: 6,
-        task: 'Add field rename to clear JsonKey\'s from code',
-        deadline: 3,
-        isDone: true,
-      ),
-      DeadlineItem(
-        id: 7,
-        task: 'Implement auto registering for injectable',
-        deadline: 3,
-        isDone: true,
+        task: 'Add ArtistsCubit tests',
+        deadline: 7,
       ),
     ];
   }
