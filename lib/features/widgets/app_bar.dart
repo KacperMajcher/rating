fbd8874652ebddd0b8676e3d57444281e83e2f25
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-10 14:47:57 +0100

    Move widgets from the homepage to other folders

---
+++ lib/features/widgets/app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Deadlines'),
+      backgroundColor: const Color.fromRGBO(41, 41, 41, 1),
+      elevation: 0,
+    );
+  }
+}
