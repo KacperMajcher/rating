8e386a4c9cda34cfc026f183b8f19ff180c1dc69
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-11 15:11:22 +0100

    Update AppBar to Material3

--- lib/features/widgets/app_bar.dart
+++ lib/features/widgets/app_bar.dart
@@ -11,9 +11,23 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text('Deadlines'),
+      title: const Text(
+        'Deadlines',
+        style: TextStyle(color: Colors.white),
+      ),
       backgroundColor: const Color.fromRGBO(41, 41, 41, 1),
       elevation: 0,
+      leading: Builder(
+        builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(Icons.menu, color: Colors.white),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          );
+        },
+      ),
     );
   }
 }
