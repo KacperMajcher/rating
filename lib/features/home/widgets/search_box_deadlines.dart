2919a211524f0c890468086984878e86a47ffdc6
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-12 17:24:43 +0100

    Adjust the widget sizes proportionally to the device screen - SearchBoxDeadlines

--- lib/features/home/widgets/search_box_deadlines.dart
+++ lib/features/home/widgets/search_box_deadlines.dart
@@ -7,35 +7,35 @@ class SearchBoxDeadlines extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double dw = MediaQuery.of(context).size.width;
+    final double dh = MediaQuery.of(context).size.height;
     return Container(
-      height: 48,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 15,
+      height: dh * 0.06,
+      padding: EdgeInsets.symmetric(
+        horizontal: dw * 0.04,
       ),
       decoration: BoxDecoration(
-        color: const Color.fromRGBO(61, 61, 61, 1),
+        color: const Color(0xFF3D3D3D),
         borderRadius: BorderRadius.circular(20),
       ),
       child: TextField(
         onChanged: (value) {
           context.read<HomeCubit>().filterItems(value);
         },
-        decoration: const InputDecoration(
-          contentPadding: EdgeInsets.all(0),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.zero,
           prefixIcon: Icon(
             Icons.search,
-            color: Color.fromRGBO(232, 93, 4, 1),
-            size: 20,
+            color: const Color(0xFFE85D04),
+            size: dh * 0.024,
           ),
           prefixIconConstraints: BoxConstraints(
-            maxHeight: 20,
-            minWidth: 25,
+            minWidth: dw * 0.06,
           ),
           border: InputBorder.none,
           hintText: 'Search',
-          hintStyle: TextStyle(
-              color: Color.fromRGBO(155, 155, 149, 1),
-              fontStyle: FontStyle.normal),
+          hintStyle: const TextStyle(
+              color: Color(0xFF9B9B95), fontStyle: FontStyle.normal),
         ),
       ),
     );
